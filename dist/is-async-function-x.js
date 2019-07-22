257d055158a6aaca8113ff9f3d3fc13e7ecd5f97
Graham Fairweather <xotic750@gmail.com>
2019-07-22 15:21:22 +0200

    :bookmark: v2.0.5
    Dependency updates

--- dist/is-async-function-x.js
+++ dist/is-async-function-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2017",
-  "date": "2019-07-21T23:20:50.769Z",
+  "date": "2019-07-22T13:21:16.690Z",
   "describe": "",
   "description": "Determine if a function is a native aync function.",
   "file": "is-async-function-x.js",
   "hash": "f212f06c7eb90e19a42c",
   "license": "MIT",
-  "version": "2.0.4"
+  "version": "2.0.5"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
